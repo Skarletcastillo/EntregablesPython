7ee672b544b8cbbd3d44fc4cb4703d749f679f3e
SkarletCastillo <genessisskarlet@gmail.com>
2025-04-10 21:54:30 -0500

    Actividad 1 y 2

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -1,5 +1,5 @@
-import streamlit as st # type: ignore
-
+import streamlit as st
+import pandas as pd
 # Configuración de la página
 st.set_page_config(   
     page_icon="📌",
@@ -8,24 +8,41 @@ st.set_page_config(
 
 st.title("Momento 2 - Actividad 1")
 
-st.header("Descripción de la actividad")
-st.markdown("""
-Esta actividad es una introducción práctica a Python y a las estructuras de datos básicas.
-En ella, exploraremos los conceptos fundamentales de Python y aprenderemos a utilizar variables,
-tipos de datos, operadores, y las estructuras de datos más utilizadas como listas, tuplas,
-diccionarios y conjuntos.
-""")
-
-
-
-st.header("Objetivos de aprendizaje")
 
 st.markdown("""
-- Comprender los tipos de datos básicos en Python
-- Aprender a utilizar variables y operadores
-- Dominar las estructuras de datos fundamentales
-- Aplicar estos conocimientos en ejemplos prácticos
+
 """)
 
-st.header("Solución")
+st.header("Actividad #1 - Creación de DataFrames")
+st.subheader("Descripción de la actividad")
+st.markdown("""
+En esta actividad realizaré 2 Dataframes, uno de un diccionario y otro de una lista de diccionarios.
+El primer DataFrame contendrá información sobre libros, incluyendo el título, autor, año de publicación y género, sobre el segundo mostrará información sobre ciudades, incluyendo el nombre, población y país.
+""")
 
+libros = {
+    "Título del libro": ["El Principito", "Don Quijote de la Mancha", "Cien años de soledad", "Los Juegos del Hambre"],
+    "Autor": ["Antoine de Saint-Exupéry", "Miguel de Cervantes", "Gabriel García Márquez", "Suzanne Collins"],
+    "Año de publicación": [1943, 1605, 1967, 2008],
+    "Género": ["Fábula, Fantasía", "Novela", "Realismo mágico", "Ciencia ficción"],
+}
+
+dataframe_libros = pd.DataFrame(libros)
+
+st.write("DataFrame de libros:")
+st.dataframe(dataframe_libros)
+
+
+st.expander("Mostrar DataFrame de ciudades", expanded=True)
+
+
+ciudades =[
+    {"Nombre": "Medellín", "Población": 58964852, "País": "Colombia"},
+    {"Nombre": "Madrid", "Población": 6589745, "País": "España"},
+    {"Nombre": "Seul", "Población": 35621000, "País": "Corea del Sur"},
+]
+
+dataframe_ciudades = pd.DataFrame(ciudades)
+
+st.write("DataFrame de ciudades:")
+st.dataframe(dataframe_ciudades)
--- pages/2_📌_M2 Actvidad 2.py
+++ pages/2_📌_M2 Actvidad 2.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import io
+import csv
 
 # Configuración de la página
 st.set_page_config(   
@@ -16,14 +19,48 @@ tipos de datos, operadores, y las estructuras de datos más utilizadas como list
 diccionarios y conjuntos.
 """)
 
-st.header("Objetivos de aprendizaje")
+st.header("Actividad #2 ")
 
 st.markdown("""
-- Comprender los tipos de datos básicos en Python
-- Aprender a utilizar variables y operadores
-- Dominar las estructuras de datos fundamentales
-- Aplicar estos conocimientos en ejemplos prácticos
+
 """)
+#Aquí es donde se carga al data y se muestra
+st.subheader("Dataset.")
+dataframe_estudiantes_colombia = pd.read_csv('static/datasets/estudiantes_colombia.csv')
+st.dataframe(dataframe_estudiantes_colombia)
 
-st.header("Solución")
+#Acá solo se pide que muestre las primeras 5 filas
+st.subheader("Ver las primeras 5 filas.")
+st.write(dataframe_estudiantes_colombia.head())
 
+#Acá solo se pide que muetsre las últimas 5 filas
+st.subheader("Ver las últimas 5 filas.")
+st.write(dataframe_estudiantes_colombia.tail())
+
+#Esto muestra características especificas de las columnas numéricas.
+st.header("Resumen.")
+st.subheader("Resumen con 'Describe'.")
+descripcion = dataframe_estudiantes_colombia.describe()
+st.write(descripcion)
+
+#Este nos muestra la información del dataframe, cuantas columnas tiene, su tipo, la cantidad de datos que hay en cada una y así.
+st.subheader("Resumen con 'Info'.")
+info = dataframe_estudiantes_colombia.info()
+buffer = io.StringIO()
+dataframe_estudiantes_colombia.info(buf=buffer)
+info_string = buffer.getvalue()
+st.code(info_string)
+
+#Acá solo se pide mostrar 3 columnas puntuales de las que hay en el dataframe.
+st.subheader("Selección de columnas: Nombre, Edad y Promedio.")
+st.write(dataframe_estudiantes_colombia[['Nombre', 'Edad', 'Promedio']])
+
+
+#Este nos filtra por medio de una barra, donde se puede buscar el promedio a filtrar y nos muestra los estudiantes que corresponden al promedio selecciondo.
+st.subheader("Filtrar estudiantes por promedio.")
+promedio = st.slider("Selecciona el promedio:", 0.0, 5.0, 3.0)
+
+filtrado_de_promedio = dataframe_estudiantes_colombia[dataframe_estudiantes_colombia['Promedio'] >= promedio]
+
+st.write("Estudiantes con promedio mayor o igual a:", promedio)
+st.dataframe(filtrado_de_promedio)
